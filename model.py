f1b833c143f42fb04c35a640c9f72dc6c1d0e7d8
spallas <davide.spallaccini@gmail.com>
2019-01-15 16:54:00 +0100

    Implement encoder operations in graph mode.
    Co-authored-by: beabevi <29659657+beabevi@users.noreply.github.com>
    Co-authored-by: Angelogeb <Angelogeb@users.noreply.github.com>

--- model.py
+++ model.py
@@ -1,15 +1,6 @@
-
 import tensorflow as tf
-import numpy as np
-import random
-import threading
 
-from tensorflow.contrib.rnn.python.ops.core_rnn_cell import RNNCell
-from tensorflow.python.ops.rnn_cell_impl import LSTMStateTuple
-from tensorflow.python.ops import variable_scope as vs, math_ops, init_ops
-from tensorflow.python.ops.math_ops import tanh, sigmoid
-from tensorflow.python.ops import array_ops
-from tensorflow.python.util import nest
+from tensorflow.python.ops.rnn_cell_impl import LSTMCell
 
 
 class TimeAttnModel:
@@ -18,11 +9,11 @@ class TimeAttnModel:
         self.config = config
 
         self.driving_series = tf.placeholder(tf.float32, [None,  # batch size
-                                                          None,  # T (length of a time window)
-                                                          None])  # n (number of supporting series)
+                                                          None,  # n (number of supporting series)
+                                                          None])  # T (length of a time window)
         self.past_history = tf.placeholder(tf.float32, [None,  # batch size
                                                         None,  # T
-                                                        1])    # y_i
+                                                        1])  # y_i
 
         self.predictions, self.loss = self.get_predictions_and_loss(self.driving_series,
                                                                     self.past_history)
@@ -44,115 +35,62 @@ class TimeAttnModel:
         self.train_op = optimizer.apply_gradients(zip(gradients, trainable_params), global_step=self.global_step)
 
     def get_predictions_and_loss(self, driving_series, past_history):
-
         # define encoder
 
+        with tf.variable_scope("EncoderRNN"):
+            cell = LSTMCell(self.config["m"], state_is_tuple=True)
+
+            initial_state = cell.zero_state(self.config["batch_size"], tf.float32)
+            state = initial_state
+            c, h = state
+            outputs = []
+
+            v_e = tf.get_variable("v_e", [self.config["batch_size"], 1,
+                                          self.config["T"]])
+
+            W_e = tf.get_variable("W_e", [self.config["batch_size"], 1,
+                                          self.config["T"], 2 * self.config["m"]])
+
+            U_e = tf.get_variable("U_e", [self.config["batch_size"], 1,
+                                          self.config["T"], self.config["T"]])
+
+            v_e = tf.tile(v_e, [1, self.config["n"], 1])
+            U_e = tf.tile(U_e, [1, self.config["n"], 1, 1])
+            W_e = tf.tile(W_e, [1, self.config["n"], 1, 1])
+
+            for t in range(self.config["T"]):
+                # if t > 0: tf.get_variable_scope().reuse_variables()
+
+                attn_input = tf.expand_dims(tf.tile(tf.concat([c, h], axis=1), [1, self.config["n"]]), -1)
+                driving_series = tf.expand_dims(driving_series, -1)
+
+                # @ is matrix multiplication
+                z = tf.tanh(tf.squeeze(W_e @ attn_input) + tf.squeeze(U_e @ driving_series))
+
+                e_t = tf.reduce_sum(v_e * z, axis=-1)
+                alpha = tf.nn.softmax(e_t)  # attention weights
+
+                driving_series = tf.squeeze(driving_series)
+                x_tilde = alpha * driving_series[:, :, t]  # input weighted with attention weights
+
+                (cell_output, state) = cell(x_tilde, state)
+                c, h = state
+                outputs.append(h)
+
+        encoder_outputs = tf.concat(outputs, axis=1)
+        encoder_outputs = tf.reshape(encoder_outputs, [self.config["batch_size"], self.config["T"], -1])
 
         # define decoder
+        with tf.variable_scope("DecoderRNN"):
+            cell = LSTMCell(self.config["m"], state_is_tuple=True)
+
+            initial_state = cell.zero_state(self.config["batch_size"], tf.float32)
+            state = initial_state
+            c, h = state
+            outputs = []
+
+            for t in range(self.config["T"]):
+                # if t > 0: tf.get_variable_scope().reuse_variables()
+                pass
 
         return []
-
-
-class CustomLSTMCell(RNNCell):
-    """
-    LSTM basic cell with attention mechanism
-    """
-
-    def __init__(self, num_units, attention_size, forget_bias=1.0, activation=tanh):
-        """Initialize the basic LSTM cell.
-        Args:
-          num_units: int, The number of units in the LSTM cell.
-          forget_bias: float, The bias added to forget gates (see above).
-          activation: Activation function of the inner states.
-        """
-        self._num_units = num_units
-        self._T = attention_size
-        self._forget_bias = forget_bias
-        self._state_is_tuple = True
-        self._activation = activation
-
-    @property
-    def state_size(self):
-        return (LSTMStateTuple(self._num_units, self._num_units)
-                if self._state_is_tuple else 2 * self._num_units)
-
-    @property
-    def output_size(self):
-        return self._num_units
-
-    def compute_output_shape(self, input_shape):
-        return self.output_size()
-
-    def __call__(self, inputs, state, scope=None):
-        """Long short-term memory cell (LSTM)."""
-        with vs.variable_scope(scope or type(self).__name__):  # "BasicLSTMCell"
-            # Parameters of gates are concatenated into one multiply for efficiency.
-            c, h = state
-
-            v_e = tf.get_variable("v_e", [self._num_units])
-            W_e = tf.get_variable("W_e", [self._T, 2 * self._num_units])
-            U_e = tf.get_variable("U_e", [self._T, self._T])
-
-            z = math_ops.matmul(W_e, array_ops.concat(1, [h, c])) + math_ops.matmul(U_e, inputs)
-            e = tanh(z)
-
-            concat = _linear([inputs, h], 4 * self._num_units, True)
-            # i = input_gate, j = new_input, f = forget_gate, o = output_gate
-            i, j, f, o = array_ops.split(1, 4, concat)
-
-            new_c = (c * sigmoid(f + self._forget_bias) + sigmoid(i) * self._activation(j))
-            new_h = self._activation(new_c) * sigmoid(o)
-
-            new_state = LSTMStateTuple(new_c, new_h)
-
-            return new_h, new_state
-
-
-def _linear(args, output_size, bias, bias_start=0.0, scope=None):
-    """Linear map: sum_i(args[i] * W[i]), where W[i] is a variable.
-    Args:
-      args: a 2D Tensor or a list of 2D, batch x n, Tensors.
-      output_size: int, second dimension of W[i].
-      bias: boolean, whether to add a bias term or not.
-      bias_start: starting value to initialize the bias; 0 by default.
-      scope: VariableScope for the created subgraph; defaults to "Linear".
-    Returns:
-      A 2D Tensor with shape [batch x output_size] equal to
-      sum_i(args[i] * W[i]), where W[i]s are newly created matrices.
-    Raises:
-      ValueError: if some of the arguments has unspecified or wrong shape.
-    """
-    if args is None or (nest.is_sequence(args) and not args):
-        raise ValueError("`args` must be specified")
-    if not nest.is_sequence(args):
-        args = [args]
-
-    # Calculate the total size of arguments on dimension 1.
-    total_arg_size = 0
-    shapes = [a.get_shape().as_list() for a in args]
-    for shape in shapes:
-        if len(shape) != 2:
-            raise ValueError("Linear is expecting 2D arguments: %s" % str(shapes))
-        if not shape[1]:
-            raise ValueError("Linear expects shape[1] of arguments: %s" % str(shapes))
-        else:
-            total_arg_size += shape[1]
-
-    dtype = [a.dtype for a in args][0]
-
-    # Now the computation.
-    with vs.variable_scope(scope or "Linear"):
-        matrix = vs.get_variable(
-            "Matrix", [total_arg_size, output_size], dtype=dtype)
-        if len(args) == 1:
-            res = math_ops.matmul(args[0], matrix)
-        else:
-            res = math_ops.matmul(array_ops.concat(1, args), matrix)
-        if not bias:
-            return res
-        bias_term = vs.get_variable(
-            "Bias", [output_size],
-            dtype=dtype,
-            initializer=init_ops.constant_initializer(
-                bias_start, dtype=dtype))
-    return res + bias_term
