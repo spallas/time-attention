7b60f527bc7922983939bceb3a5fcdfdc66c09f3
Angelogeb <any__955@hotmail.it>
2019-01-16 20:03:59 +0100

    Implemented data loading
    Currently it takes a lot of time to vstack y_T. Issue still has
    to be debugged

--- data_loader.py
+++ data_loader.py
@@ -1,27 +1,102 @@
 import tensorflow as tf
+import pandas as pd
+import numpy as np
 
 tf.enable_eager_execution()
 
+
 config = {
-    "data_path": ["data/NEW-DATA-1.T15.csv", "data/NEW-DATA-2.T15.csv"],
+    "data_paths": ["data/NEW-DATA-1.T15.csv", "data/NEW-DATA-2.T15.csv"],
     "drop_cols": [
-        0,  # Date
-        1,  # Time
-        18,  # Entalpic motor 1      , All zeros
-        19,  # Entalpic motor 2      , All zeros
-        20,  # Entalpic motor turbo  , All zeros
+        "1:Date",
+        "2:Time",
+        "19:Exterior_Entalpic_1",  # All zeros
+        "20:Exterior_Entalpic_2",
+        "21:Exterior_Entalpic_turbo",
+        "24:Day_Of_Week",
     ],
+    "target_cols": ["4:Temperature_Habitacion_Sensor"],
     "sep": " ",
     "window_size": 10,
 }
 
-with open(config["data_path"][0]) as f:
-    header = f.readline().strip().split(config["sep"])
 
+def get_train_test_dataset(config, cat_before_window=False):
+    """
+    cat_before_window : whether to concatenate the files before transforming it
+                        into windows
+    """
+
+    def window(df, size, driving_series, target_series):
+        X = df[driving_series].values
+        y = df[target_series].values
+        X_T = []
+        y_T = []
+        for i in range(len(X) - size):
+            X_T.append(X[i: i + size])
+            y_T.append(y[i: i + size])
+
+        return np.array(X_T), np.array(y_T)
+
+    path = config["data_paths"][0]
+
+    with open(path) as f:
+        header = f.readline().strip().split(config["sep"])
+
+    usecols = [col for col in header if col not in config["drop_cols"]]
+
+    driving_series = [
+        col for col in usecols if col not in config["target_cols"]
+    ]
+
+    dfs = []
+    for path in config["data_paths"]:
+        dfs.append(pd.read_csv(path, sep=config["sep"], usecols=usecols))
+
+    df = None
+    X_T = None
+    y_T = None
+    if cat_before_window:
+        df = pd.concat(dfs)
+        X_T, y_T = window(
+            df, config["window_size"], driving_series, config["target_cols"]
+        )
+        X_T = X_T.transpose((0, 2, 1))
+    else:
+        X_Ts = []
+        y_Ts = []
+        for df in dfs:
+            X_T, y_T = window(
+                df, config["window_size"], driving_series, config["target_cols"]
+            )
+            X_T = X_T.transpose((0, 2, 1))
+            X_Ts.append(X_T)
+            y_Ts.append(y_Ts)
+        print("Ended transforms")
+        X_T = np.vstack(X_Ts)
+        print("Ended X_T")
+        y_T = np.vstack(y_Ts)
+        print("Ended y_T")
+
+    X_dataset = tf.data.Dataset.from_tensor_slices(X_T)
+    y_dataset = tf.data.Dataset.from_tensor_slices(y_T)
+
+    dataset = tf.data.Dataset.zip((X_dataset, y_dataset))
+    return dataset
+
+
+dataset = get_train_test_dataset(config)
+
+it = dataset.batch(3)
+
+for x, y in it:
+    print("X", x)
+    print("Y", y)
+    break
+
+"""
 record_defaults = [tf.float32] * (len(header) - len(config["drop_cols"]))
-select_cols = list(
-    filter(lambda x: x not in config["drop_cols"], range(len(header)))
-)
+
 
 data = tf.data.experimental.CsvDataset(
     config["data_path"],
@@ -31,9 +106,7 @@ data = tf.data.experimental.CsvDataset(
     header=True,
 )
 
-data_w = data.window(config["window_size"], drop_remainder=True).flat_map(
-    lambda x1, x2, x3, x4, x5, x6, x7, x8, x9, x10, x11, x12, x13, x14, x15, x16, x17, x18, x19: x1
-)
 it = data_w.make_one_shot_iterator()
 for x in it:
     print(x)
+"""
