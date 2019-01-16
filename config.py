b31398997fbbb98235175fe77a32b7607eeba711
Angelogeb <any__955@hotmail.it>
2019-01-16 22:43:10 +0100

    Fixed numpy bug (?) and implemented Config dataclass
    The Config dataclass can be used to store and retrieve configs.
    It is a better approach than plain json since we get autocompletion
    of the attributes on IDEs (freeing us from dummy bugs) and proper
    documentation.
    
    The numpy bug is resolved but there's no real explanation. By
    squeezing and then re-adding the last dummy one dimension it
    works like a charm.
    
    Co-authored-by: beabevi <29659657+beabevi@users.noreply.github.com>

---
+++ config.py
@@ -0,0 +1,90 @@
+from typing import List, Optional
+
+from dataclasses import dataclass, field
+from dataclasses_json import dataclass_json
+
+
+@dataclass_json
+@dataclass
+class Config(object):
+    """
+    Attributes
+    ----------
+    n:
+        Number of driving series
+
+    decay_rate:
+        Decay rate of the momentum
+
+    data_paths:
+        List of paths to input files. The input files can be split into
+        multiple csv as long as they have same header and format.
+        When loading the data through `get_train_test_datasets` one can
+        choose whether the csv have to be merged (when loaded) and then
+        transformed into windows or merged only after windowing. This
+        was done since the SML2010 dataset is split into two csvs with the
+        second starting at a timestep far later than the first one
+
+    target_cols:
+        The name of the columns that are the target values (y) for the
+        prediction. When the dataset is returned from
+        `get_train_test_datasets` the X will contain values of the columns in
+        (header minus (target_cols union drop_cols)) while the y values of the
+        `target_cols`.
+
+    drop_cols:
+        The name of the columns to be dropped from the csv.
+        In the case of SML2010 some columns are 0, such as the ones regarding
+        Exterior_Entalpic_*
+
+    m:
+        The size of the hidden state of the encoder
+
+    p:
+        The size of the hidden state of the decoder
+
+    sep:
+        The pattern separating columns in the csvs
+
+    T:
+        The number of past values the predictor can perceive. T values
+        of the driving series (X) and T-1 values for the target series.
+        The T-th values of the target series (y) are the ones to be predicted
+
+    learning_rate:
+        Learning rate for optimizing the parameters
+
+    decay_frequency:
+
+    batch_size:
+        Number of windows to be processed at the same time
+
+    num_epochs:
+        Number of epochs to train the network
+
+    log_dir:
+        Directory for logging
+
+    """
+
+    n: Optional[int]  # Set at runtime
+    decay_rate: float
+    data_paths: List[str]
+    target_cols: List[str]
+    drop_cols: Optional[List[str]] = field(default_factory=list)
+    m: int = 64  # TODO(xhebraj) set optimal values of static attributes
+    p: int = 16
+    sep: str = ","
+    T: int = 10
+    learning_rate: float = 0.01
+    decay_frequency: int = 100
+    batch_size: int = 4
+    num_epochs: int = 10
+    log_dir: str = "log/"
+
+
+# Test
+if __name__ == "__main__":
+    with open("conf/experiment2.json") as f:
+        c = Config.from_json(f.read())
+    print(c.to_json())
--- data_loader.py
+++ data_loader.py
@@ -2,29 +2,37 @@ import tensorflow as tf
 import pandas as pd
 import numpy as np
 
+from config import Config
+
 tf.enable_eager_execution()
 
 
-config = {
-    "data_paths": ["data/NEW-DATA-1.T15.csv", "data/NEW-DATA-2.T15.csv"],
-    "drop_cols": [
-        "1:Date",
-        "2:Time",
-        "19:Exterior_Entalpic_1",  # All zeros
-        "20:Exterior_Entalpic_2",
-        "21:Exterior_Entalpic_turbo",
-        "24:Day_Of_Week",
-    ],
-    "target_cols": ["4:Temperature_Habitacion_Sensor"],
-    "sep": " ",
-    "window_size": 10,
-}
-
-
-def get_train_test_dataset(config, cat_before_window=False):
+# TODO(xhebraj) implement return of train/test/validation
+def get_train_test_dataset(config: Config, cat_before_window=False):
     """
-    cat_before_window : whether to concatenate the files before transforming it
-                        into windows
+    Returns X and y of the data passed as config.
+
+    Parameters
+    ----------
+    config : Config
+    cat_before_window : bool
+        Whether to concatenate the files before transforming it
+        into windows
+
+    Returns
+    -------
+    d : tensorflow.data.Dataset(tuples)
+        The tuples are
+            X : (config.batch_size, config.n, config.T)
+            y : (config.batch_size, config.T, 1)
+    Usage
+    -----
+    ```
+        dataset = get_train_test_dataset(config)
+        it = dataset.batch(batch_size)
+        for x, y in it:
+            print(x, y)
+    ```
     """
 
     def window(df, size, driving_series, target_series):
@@ -33,50 +41,43 @@ def get_train_test_dataset(config, cat_before_window=False):
         X_T = []
         y_T = []
         for i in range(len(X) - size):
-            X_T.append(X[i: i + size])
-            y_T.append(y[i: i + size])
+            X_T.append(X[i : i + size])
+            y_T.append(y[i : i + size])
 
         return np.array(X_T), np.array(y_T)
 
-    path = config["data_paths"][0]
+    path = config.data_paths[0]
 
     with open(path) as f:
-        header = f.readline().strip().split(config["sep"])
+        header = f.readline().strip().split(config.sep)
 
-    usecols = [col for col in header if col not in config["drop_cols"]]
+    usecols = [col for col in header if col not in config.drop_cols]
 
-    driving_series = [
-        col for col in usecols if col not in config["target_cols"]
-    ]
+    driving_series = [col for col in usecols if col not in config.target_cols]
 
     dfs = []
-    for path in config["data_paths"]:
-        dfs.append(pd.read_csv(path, sep=config["sep"], usecols=usecols))
+    for path in config.data_paths:
+        dfs.append(pd.read_csv(path, sep=config.sep, usecols=usecols))
 
     df = None
     X_T = None
     y_T = None
     if cat_before_window:
         df = pd.concat(dfs)
-        X_T, y_T = window(
-            df, config["window_size"], driving_series, config["target_cols"]
-        )
+        X_T, y_T = window(df, config.T, driving_series, config.target_cols)
         X_T = X_T.transpose((0, 2, 1))
     else:
         X_Ts = []
         y_Ts = []
         for df in dfs:
-            X_T, y_T = window(
-                df, config["window_size"], driving_series, config["target_cols"]
-            )
+            X_T, y_T = window(df, config.T, driving_series, config.target_cols)
             X_T = X_T.transpose((0, 2, 1))
             X_Ts.append(X_T)
-            y_Ts.append(y_Ts)
-        print("Ended transforms")
+            y_Ts.append(np.squeeze(y_T))
         X_T = np.vstack(X_Ts)
-        print("Ended X_T")
         y_T = np.vstack(y_Ts)
-        print("Ended y_T")
+
+    y_T = np.expand_dims(y_T, axis=-1)
 
     X_dataset = tf.data.Dataset.from_tensor_slices(X_T)
     y_dataset = tf.data.Dataset.from_tensor_slices(y_T)
@@ -85,26 +86,21 @@ def get_train_test_dataset(config, cat_before_window=False):
     return dataset
 
 
-dataset = get_train_test_dataset(config)
+# Test
+if __name__ == "__main__":
+    with open("conf/experiment2.json") as f:
+        config = Config.from_json(f.read())
 
-it = dataset.batch(3)
+    dataset = get_train_test_dataset(config)
 
-for x, y in it:
-    print("X", x)
-    print("Y", y)
-    break
+    it = dataset.batch(config.batch_size)
+
+    for x, y in it:
+        print("X", x)
+        print("Y", y)
+        break
 
 """
-record_defaults = [tf.float32] * (len(header) - len(config["drop_cols"]))
-
-
-data = tf.data.experimental.CsvDataset(
-    config["data_path"],
-    record_defaults,
-    field_delim=config["sep"],
-    select_cols=select_cols,
-    header=True,
-)
 
 it = data_w.make_one_shot_iterator()
 for x in it:
