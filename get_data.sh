029cf76f1ab0dc04795f1e8197fb20c154ca82fd
Angelogeb <any__955@hotmail.it>
2019-01-18 11:57:23 +0100

    Removed cds from script

--- get_data.sh
+++ get_data.sh
@@ -4,41 +4,37 @@ if [ ! -d data ]; then
     mkdir data
 fi
 
-cd data
-
 # Download NASDAQ100 dataset from author's
-if [ ! -f nasdaq100.zip ]; then
+if [ ! -f data/nasdaq100.zip ]; then
     wget http://cseweb.ucsd.edu/~yaq007/nasdaq100.zip
+    mv nasdaq100.zip data
 fi
 
 # Download SML2010 dataset from UCI
-if [ ! -f NEW-DATA.zip ]; then
+if [ ! -f data/SML2010.zip ]; then
     wget https://archive.ics.uci.edu/ml/machine-learning-databases/00274/NEW-DATA.zip
+    mv NEW-DATA.zip data/SML2010.zip
 fi
 
-if [ -d SML2010 ]; then
-    rm -r SML2010
+if [ -d  data/SML2010 ]; then
+    rm -r  data/SML2010
 fi
 
-if [ -d nasdaq100 ]; then
-    rm -r nasdaq100
+if [ -d data/nasdaq100 ]; then
+    rm -r data/nasdaq100
 fi
 
 # SML2010 Processing
-mv NEW-DATA.zip SML2010.zip
-unzip SML2010.zip -q -d SML2010
-cd SML2010
+unzip -q data/SML2010.zip -d data/SML2010
 
 # Remove first 4 chars of each file making it a proper csv
-tail -c +4 NEW-DATA-1.T15.txt > NEW-DATA-1.T15.csv
-tail -c +4 NEW-DATA-2.T15.txt > NEW-DATA-2.T15.csv
+tail -c +4 data/SML2010/NEW-DATA-1.T15.txt > data/SML2010/NEW-DATA-1.T15.csv
+tail -c +4 data/SML2010/NEW-DATA-2.T15.txt > data/SML2010/NEW-DATA-2.T15.csv
 
 # Clean
-rm NEW-DATA-1.T15.txt
-rm NEW-DATA-2.T15.txt
-
-cd ..
+rm data/SML2010/NEW-DATA-1.T15.txt
+rm data/SML2010/NEW-DATA-2.T15.txt
 
 # NASDAQ100 Processing
-unzip -q qnasdaq100.zip
-rm -r __MACOSX
+unzip -q data/nasdaq100.zip -d data
+rm -r data/__MACOSX
